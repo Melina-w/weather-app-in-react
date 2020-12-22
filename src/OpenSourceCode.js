8e093ac992070060ea90eb1e71f6ca39feaf4001
MELINA <meluwaigant@gmail.com>
2020-12-23 12:56:12 +1300

    Update Githug Url

--- src/OpenSourceCode.js
+++ src/OpenSourceCode.js
@@ -4,7 +4,7 @@ export default function OpenSourceCode() {
   return (
     <div class="col-12 credits">
       <a
-        href="https://github.com/Melina-w/my-weather-app-project"
+        href="https://github.com/Melina-w/weather-app-in-react"
         target="_blank"
         rel="noreferrer"
       >
